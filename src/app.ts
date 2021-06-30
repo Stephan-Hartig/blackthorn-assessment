b785b38440ac5b3f45a1353b152e0b4db8cd4b57
Stephan Hartig <the.stephan.hartig@gmail.com>
2021-06-29 22:15:14 -0400

    Testing sql server

--- src/app.ts
+++ src/app.ts
@@ -52,10 +52,9 @@ conn.connect((err) => {
    else
       console.log(`Opened MySQL connection.`);
 });
-//
-//
-//
-conn.query('create table Foo (FooID int not null auto_increment primary key);', (err, res) => {
+
+//conn.query('create table Foo (FooID int not null auto_increment primary key);', (err, res) => {
+conn.query('show tables;', (err, res) => {
    console.log(JSON.stringify(res, null, '  '));
 });
 
