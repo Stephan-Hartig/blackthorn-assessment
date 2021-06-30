58a2080f851bbe702a10b27d17299afd56692ba5
Stephan Hartig <the.stephan.hartig@gmail.com>
2021-06-30 10:49:19 -0400

    Finished the sql table creation script

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import * as path from 'path';
-import * as mysql from 'mysql';
+import * as mysql from 'mysql2';
+
+
+
+/* Load environment vars. */
 
 function exit(reason: string): any {
    console.log('Error: ' + reason);
@@ -12,31 +16,28 @@ const PORT = process.env.PORT ?? 5000;
 
 const MYSQL_URL = process.env.JAWSDB_URL
    ?? exit('Env var for db url not found.');
-const MYSQL_USER = process.env.JAWSDB_USER
-   ?? exit('Env var for db user not found.');
-const MYSQL_PASSWORD = process.env.JAWSDB_PASSWORD
-   ?? exit('Env var for db password not found.');
-const MYSQL_DATABASE = process.env.JAWSDB_DATABASE
-   ?? exit('Env var for db database not found.');
+
+
+
+/* Connect to the mysql server. */
 
 let conn: mysql.Connection;
 
 if (process.env.NODE_ENV === 'production') {
    console.log('Deployment mode detected: Will connect to ClearDB database.');
    console.log('TODO: Connect via ssl.');
+
    conn = mysql.createConnection(MYSQL_URL);
-   //conn = mysql.createConnection({
-   //   host:       MYSQL_URL,
-   //   user:       MYSQL_USER,
-   //   password:   MYSQL_PASSWORD,
-   //   database:   MYSQL_DATABASE
-   //   ssl: {
-   //      ca: fs.readFileSync(__dirname + '/mysql-ca.crt');
-   //   }
-   //});
 }
 else {
    console.log('Local mode detected: Will connect to local database.');
+
+   const MYSQL_USER = process.env.JAWSDB_USER
+      ?? exit('Env var for db user not found.');
+   const MYSQL_PASSWORD = process.env.JAWSDB_PASSWORD
+      ?? exit('Env var for db password not found.');
+   const MYSQL_DATABASE = process.env.JAWSDB_DATABASE
+      ?? exit('Env var for db database not found.');
 
    conn = mysql.createConnection({
       host:       MYSQL_URL,
@@ -45,13 +46,6 @@ else {
       database:   MYSQL_DATABASE
    });
 }
-
-conn.connect((err) => {
-   if (err)
-      console.error('Error connecting: ' + err.stack);
-   else
-      console.log(`Opened MySQL connection.`);
-});
 
 conn.query('show tables;', (err, res) => {
    console.log(JSON.stringify(res, null, '  '));
