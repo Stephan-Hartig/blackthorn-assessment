c71afc5bd6f407400ef26e770e020fdd8e1a79f8
Stephan Hartig <the.stephan.hartig@gmail.com>
2021-06-29 20:05:59 -0400

    Added local mode

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,45 @@ import express from 'express';
 import * as path from 'path';
 import * as mysql from 'mysql';
 
-//function exit(reason: string): any {
-//   console.log('Error: ' + reason);
-//   console.log('Exiting...');
-//   process.exit(1);
-//}
-//
+function exit(reason: string): any {
+   console.log('Error: ' + reason);
+   console.log('Exiting...');
+   process.exit(1);
+}
+
 const PORT = process.env.PORT || 5000;
-//const MYSQL_URL = process.env.CLEARDB_DATABASE_URL
-//   ?? exit('Env var for db url not found.');
-//const MYSQL_USER = process.env.CLEARDB_DATABASE_USERNAME
-//   ?? exit('Env var for db username not found.');
-//const MYSQL_PASS = process.env.CLEARDB_DATABASE_PASSWORD
-//   ?? exit('Env var for db password not found.');
-//
-//const conn = mysql.createConnection({
-//   host:       MYSQL_URL,
-//   user:       MYSQL_USER,
-//   password:   MYSQL_PASS,
-//   database:   'Shopping'
-//});
+
+const MYSQL_URL = process.env.CLEARDB_DATABASE_URL
+   ?? exit('Env var for db url not found.');
+const MYSQL_USER = process.env.CLEARDB_DATABASE_USER
+   ?? exit('Env var for db user not found.');
+const MYSQL_PASSWORD = process.env.CLEARDB_DATABASE_PASSWORD
+   ?? exit('Env var for db password not found.');
+const MYSQL_DATABASE = process.env.CLEARDB_DATABASE_DATABASE
+   ?? exit('Env var for db database not found.');
+
+let conn: mysql.Connection;
+
+if (MYSQL_URL === 'localhost') {
+   console.log('Local mode detected: Will connect to local database.');
+
+   conn = mysql.createConnection({
+      host:       MYSQL_URL,
+      user:       MYSQL_USER,
+      password:   MYSQL_PASSWORD,
+      database:   MYSQL_DATABASE
+   });
+}
+else {
+   console.log('Deployment mode detected: Will connect to ClearDB database.');
+//   conn = mysql.createConnection({
+//      host: MYSQL_URL,
+//      ssl: {
+//         ca: fs.readFileSync(__dirname + '/mysql-ca.crt');
+//      }
+//   });
+}
+
 
 //conn.connect(() => { console.log(`Opened MySQL connection on port ${PORT}.`); });
 
